b42a8d483b110b72424bd0f153216362e12bab21
gokuqa <59706051+gokuqa@users.noreply.github.com>
2022-08-18 21:36:17 -0500

    Update nuevocliente.js

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -1,38 +1,117 @@
-(function () {
-  let DB;
+(function() {
+    let DB;
 
-  const formulario = document.querySelector("#formulario");
+    const formulario = document.querySelector('#formulario');
 
-  document.addEventListener("DOMContentLoaded", () => {
-    formulario.addEventListener("submit", validarCliente);
-    conectarDB();
-  });
- 
+    document.addEventListener('DOMContentLoaded', () => {
+        formulario.addEventListener('submit', validarCliente);
 
-  function validarCliente(e) {
-    e.preventDefault();
-    //leer inputs.
+        conectarDB();
+    });
 
-    const nombre = document.querySelector("#nombre").value;
-    const email = document.querySelector("#email").value;
-    const telefono = document.querySelector("#telefono").value;
-    const empresa = document.querySelector("#empresa").value;
+    function conectarDB() {
+        // ABRIR CONEXIÓN EN LA BD:
 
-    if (nombre === "" || email === "" || telefono === "" || empresa === "") {
-      imprimirAlerta("todos los campos son obligatorios", "error");
-      return;
+        let abrirConexion = window.indexedDB.open('crm', 1);
+
+        // si hay un error, lanzarlo
+        abrirConexion.onerror = function() {
+            console.log('Hubo un error');
+        };
+    
+        // si todo esta bien, asignar a database el resultado
+        abrirConexion.onsuccess = function() {
+            // guardamos el resultado
+            DB = abrirConexion.result;
+        };
     }
-    //crer un objeto para la informacion.
 
-    const cliente = {
-      nombre,
-      email,
-      telefono,
-      empresa,
-      id: Date.now(),
-    };
-    crearNuevoCliente(cliente);
-  }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if(nombre === '' || email === '' || telefono === '' || empresa === '') {
+             
+
+            return;
+        }
+
+        // añadir a la BD...
+        // crear un nuevo objeto con toda la info
+
+        const cliente = {
+            nombre, 
+            email,
+            telefono,
+            empresa
+        };
+
+        // Generar un ID único
+        cliente.id = Date.now();
+
+
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente) {
+
+        
+
+        // NUEVO: 
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        // console.log(objectStore);
+        objectStore.add(cliente);
+
+        transaction.oncomplete = () => {
+            console.log('Cliente Agregado');
+
+            // Mostrar mensaje de que todo esta bien...
+            imprimirAlerta('Se agregó correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        };
+
+        transaction.onerror = () => {
+            console.log('Hubo un error!');
+            imprimirAlerta('Hubo un Error', 'error');
+        };
+    }
+
+    function imprimirAlerta(mensaje, tipo) {
+         // Crea el div
+
+         const divMensaje = document.createElement('div');
+         divMensaje.classList.add( "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center" );
+
+         if(tipo === 'error') {
+            divMensaje.classList.add('bg-red-100', "border-red-400", "text-red-700");
+         } else {
+             divMensaje.classList.add('bg-green-100', "border-green-400", "text-green-700");
+         }
+         
+         // Mensaje de error
+         divMensaje.textContent = mensaje;
+ 
+         // Insertar en el DOM
+        formulario.appendChild(divMensaje);
+ 
+         // Quitar el alert despues de 3 segundos
+         setTimeout( () => {
+             divMensaje.remove();
+         }, 3000);
+    }
+
+})();
   function crearNuevoCliente(cliente) {
     const transaction = DB.transaction(["crm"], "readwrite");
     const objectStore = transaction.objectStore("crm");
